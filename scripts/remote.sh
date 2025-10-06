355af3ef08388bfcd55822deac3b5c2c15dfe757
Ryan Bunney <ryanbunney@fastmail.com>
2025-10-06 16:24:10 +0800

    Add remote-setup option to Makefile
    This creates all the necessary directories for running DALiuGE on Setonix, installing into a virtualenv in the /software/projects/ space and creating scratch directory if it does not exist.

---
+++ scripts/remote.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Setup environment
+
+DEFAULT_INSTALL="/software/projects/pawsey0411/daliuge"
+read -p "Create directories in default directory ($DEFAULT_INSTALL) (y/n): " RESPONSE
+if [ "${RESPONSE}" == 'y' ]; then
+  INSTALL_DIR=${DEFAULT_INSTALL}
+else
+  read -p "Please enter alternate directory: " INSTALL_DIR
+fi
+
+mkdir ${INSTALL_DIR}
+cd $INSTALL_DIR
+
+# Make virtual environment
+python -m venv dlg_env
+source dlg_env/bin/activate
+
+# Install dependencies
+cd ../
+make install
+
+deactivate
+
+# Setup scratch if not already setup
+
+DEFAULT_SCRATCH="/scratch/pawsey0411/chiles-daliuge"
+mkdir DEFAULT_SCRATCH
+
+
